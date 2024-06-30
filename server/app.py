070948c11958ff95e0e4f40e9fdac3a13eb43ad2
Bilha <bilha.leposo@student.moringaschool.com>
2024-06-30 18:01:28 +0300

    completes lab

--- server/app.py
+++ server/app.py
@@ -46,6 +46,23 @@ class PlantByID(Resource):
     def get(self, id):
         plant = Plant.query.filter_by(id=id).first().to_dict()
         return make_response(jsonify(plant), 200)
+    
+    def patch(self, id):
+        data = request.get_json()
+        plant = Plant.query.filter_by(id=id).first_or_404()
+
+        if 'is_in_stock' in data:
+            plant.is_in_stock = data['is_in_stock']
+
+        db.session.commit()
+        return make_response(plant.to_dict(), 200)
+
+    def delete(self, id):
+        plant = Plant.query.filter_by(id=id).first_or_404()
+        db.session.delete(plant)
+        db.session.commit()
+        return '', 204
+
 
 
 api.add_resource(PlantByID, '/plants/<int:id>')
